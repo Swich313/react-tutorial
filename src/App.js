406e6800558cc237671d759125d566586ee4864e
Andrey <andruha313@gmail.com>
2022-01-16 15:48:27 +0200

    Second

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoList from './components/TodoList';
+import {useState} from 'react'
 
 function App() {
+  const [todos, setTodos] = useState(['Todo 1', "todo 2"]);
+
+  function createTodo () {
+
+  }
+
   return (
     <div className="App">
-      <div>ПТН ПНХ</div>
-      
+      <input type = "text" />
+      <button onClick={createTodo}>Click</button>
+      <TodoList todosToList={todos} />
     </div>
   );
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function TodoList({todosToList}) {
+
+    console.log(todosToList)
+
+    return (
+        <div>
+            Hello From todo List: {todosToList}  
+        </div>
+    )
+}
